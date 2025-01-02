d16c874cd62218801cf0e2dede25589d77dc4597
ahmad <ahmdgamrs84@gmail.com>
2025-01-02 20:14:26 +0300

    clean version, done invoice creation styling

---
+++ .history/src/components/ReusableComponents/inputs/DropdownInput_20250102200844.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  title: String,
+})
+</script>
+<template>
+  <div class="">
+    <select
+      id="countries"
+      placeholder="hehe"
+      class="bg-gray-50 block border border-gray-300 size-full text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5"
+    >
+      <slot> </slot>
+    </select>
+  </div>
+</template>
---
+++ .history/src/components/ReusableComponents/inputs/NumbersInput_20241221070313.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center">
+    <input
+      type="number"
+      id="first_product"
+      class="bg-gray-50 p-1 size-full border rounded-lg border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="1"
+      required
+    />
+  </div>
+</template>
